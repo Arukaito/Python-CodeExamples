8bafb2628a2f6a2a45e2d03f49ddc04c7e0d9be1
Alkaid Darooga <alkaid_dlarosa@hotmail.com>
2017-09-14 09:55:25 -0500

    Generamos el requeriment.txt

---
+++ Py/Priority Queue Operations.py
@@ -0,0 +1,4 @@
+#Problema. Necesitamos implementar una cola que tenga la capicidad de dar prioridad al objeto que tenga un valor mas alto en la lista de prioridades
+
+#Solucion: Implementar un sistema de Cola con prioridades de Objetos
+
